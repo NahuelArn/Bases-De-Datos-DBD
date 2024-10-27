3b4d035a7c81ae9ea3b64715c7db5c728976dbbc
nahuelarn <nahuelarancibia4@gmail.com>
2024-10-27 03:28:58 -0300

    FX

--- Practicas-Resueltas/tp4_sql/eje12/eje6.sql
+++ Practicas-Resueltas/tp4_sql/eje12/eje6.sql
@@ -5,8 +5,8 @@
 DELETE FROM Atencion WHERE nroCliente IN (
     SELECT c.nroCliente
     FROM Cliente c
-    WHERE dni= '22222222'
+    WHERE dni = '22222222'
 )
 
 
-DELETE FROM Cliente WHERE dni= '22222222';
+DELETE FROM Cliente WHERE dni = '22222222';
